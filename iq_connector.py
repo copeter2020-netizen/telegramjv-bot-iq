170353fea4265dfc38e62f5451d5f828cd89fab3
copeter2020-netizen <copeter2020@gmail.com>
2026-03-05 18:59:20 -0500

    Improve connection stability with retry mechanism
    Refactor connection handling to include retries and reconnection logic.

--- iq_connector.py
+++ iq_connector.py
@@ -1,22 +1,50 @@
 from iqoptionapi.stable_api import IQ_Option
 import os
+import time
 
 class ConectorIQ:
 
     def __init__(self):
+
         self.email = os.getenv("IQ_EMAIL")
         self.password = os.getenv("IQ_PASSWORD")
+
         self.API = IQ_Option(self.email, self.password)
 
     def conectar(self):
-        self.API.connect()
 
-        if self.API.check_connect():
-            print("Conectado a IQ Option")
-        else:
-            print("Error conectando")
+        while True:
 
-        self.API.change_balance("PRACTICE")
+            try:
 
-    def velas(self, par, timeframe=60, cantidad=50):
-        return self.API.get_candles(par, timeframe, cantidad, self.API.get_server_timestamp())
+                print("Conectando a IQ Option...")
+
+                self.API.connect()
+
+                if self.API.check_connect():
+                    print("Conectado correctamente")
+                    break
+
+            except:
+                print("Error conectando, reintentando...")
+
+            time.sleep(5)
+
+    def verificar_conexion(self):
+
+        if not self.API.check_connect():
+
+            print("Reconectando...")
+
+            self.conectar()
+
+    def velas(self, par):
+
+        self.verificar_conexion()
+
+        return self.API.get_candles(
+            par,
+            60,
+            100,
+            self.API.get_server_timestamp()
+        )
