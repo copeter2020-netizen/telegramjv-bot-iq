f5863d6791c41c35ad5a3a52eb0f636abaf3368a
copeter2020-netizen <copeter2020@gmail.com>
2026-03-06 03:29:07 -0500

    Refactor ConectorIQ to use instance variables

--- iq_connector.py
+++ iq_connector.py
@@ -1,71 +1,33 @@
 from iqoptionapi.stable_api import IQ_Option
-import os
 import time
-
 
 class ConectorIQ:
 
-    def __init__(self):
+    def __init__(self, email, password):
 
-        email = os.getenv("IQ_EMAIL")
-        password = os.getenv("IQ_PASSWORD")
-
-        self.API = IQ_Option(email, password)
+        self.email = email
+        self.password = password
+        self.api = None
 
     def conectar(self):
 
-        while True:
-
-            try:
-
-                print("Conectando a IQ Option...")
-
-                self.API.connect()
-
-                if self.API.check_connect():
-
-                    print("Conectado correctamente")
-
-                    break
-
-            except Exception as e:
-
-                print("Error conectando:", e)
-
-            time.sleep(5)
-
-    def velas(self, par):
-
         try:
 
-            velas = self.API.get_candles(
-                par,
-                60,
-                100,
-                self.API.get_server_timestamp()
-            )
+            self.api = IQ_Option(self.email, self.password)
 
-            return velas
+            self.api.connect()
+
+            if self.api.check_connect():
+
+                print("✅ Conectado a IQ Option")
+                return True
+
+            else:
+
+                print("❌ No se pudo conectar a IQ Option")
+                return False
 
         except Exception as e:
 
-            print("Error obteniendo velas:", e)
-            return None
-
-
-    # NUEVA FUNCIÓN
-    def obtener_pares_abiertos(self):
-
-        activos = self.API.get_all_open_time()
-
-        pares_otc = []
-
-        for par in activos["binary"]:
-
-            if "OTC" in par:
-
-                if activos["binary"][par]["open"]:
-
-                    pares_otc.append(par)
-
-        return pares_otc 
+            print("Error conectando:", e)
+            return False 
